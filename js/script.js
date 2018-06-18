825e21ac38023a8dbc61097b76a735e01ac8190c
Dante Galindo Cruz <galindodantem@gmail.com>
2018-06-18 16:34:58 -0400

    Add files for mock site

---
+++ js/script.js
@@ -0,0 +1,13 @@
+hamburgerMenu = document.querySelector('#hamburger-menu');
+hamburgerLines = document.querySelectorAll('.hamburger');
+leftMenu = document.querySelector('#left-menu');
+
+hamburgerMenu.addEventListener('click', function(e){
+  leftMenu.style.transition = "width 2s";
+  leftMenu.style.width = 0;
+  hamburgerMenu.style.position='fixed';
+  hamburgerMenu.style.width='2%';
+  for(var i=0; i < hamburgerLines.length; i++){
+  hamburgerLines[i].style.borderBottom="2px solid #000";
+ }
+})
