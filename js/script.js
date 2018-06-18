6caf93104bb83e68d3538acb11db835f6139407b
Dante Galindo Cruz <galindodantem@gmail.com>
2018-06-18 17:54:05 -0400

    Change css

--- js/script.js
+++ js/script.js
@@ -3,11 +3,12 @@ hamburgerLines = document.querySelectorAll('.hamburger');
 leftMenu = document.querySelector('#left-menu');
 
 hamburgerMenu.addEventListener('click', function(e){
-  leftMenu.style.transition = "width 2s";
-  leftMenu.style.width = 0;
-  hamburgerMenu.style.position='fixed';
-  hamburgerMenu.style.width='2%';
-  for(var i=0; i < hamburgerLines.length; i++){
-  hamburgerLines[i].style.borderBottom="2px solid #000";
- }
+    leftMenu.style.transition = "width 2s";
+    leftMenu.style.width= 0;
+    hamburgerMenu.style.position='fixed';
+    hamburgerMenu.style.left = '5px';
+    hamburgerMenu.style.width = '2%';
+    for(var i=0; i < hamburgerLines.length; i++){
+    hamburgerLines[i].style.borderBottom="2px solid #000";
+   }
 })
