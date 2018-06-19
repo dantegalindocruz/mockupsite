66fc0f1241dbf8c247cffd028fc0c2250511d562
Dante Galindo Cruz <galindodantem@gmail.com>
2018-06-19 11:36:23 -0400

    adding styles to the main content

--- js/script.js
+++ js/script.js
@@ -2,14 +2,23 @@ hamburgerMenu = document.querySelector('.hamburger-menu');
 //hamburgerLines = document.querySelectorAll('.hamburger');
 leftMenu = document.querySelector('#left-menu');
 mainContentMenu = document.querySelector('#main-content .hamburger-menu');
+mainContentContainer = document.querySelector('#main-content .container');
 filter = document.querySelector('#filter');
 
 hamburgerMenu.addEventListener('click', function(e){
     leftMenu.style.transition = "transform 1s ease";
     leftMenu.style.transform= "translate(-150%)";
+    mainContentContainer.style.transition = "margin 1s ease";
+    mainContentContainer.style.margin = "0 15%";
+    mainContentContainer.style.transition = "width 1s ease";
+    mainContentContainer.style.width= "70%";
 });
 
 mainContentMenu.addEventListener('click', function(e){
   leftMenu.style.transition = "transform 1s ease";
   leftMenu.style.transform= "translate(0)";
+  mainContentContainer.style.transition = "margin 1s ease";
+  mainContentContainer.style.margin = " 0 10%";
+  mainContentContainer.style.transition = "width 1s ease";
+  mainContentContainer.style.width= "60%";
 });
