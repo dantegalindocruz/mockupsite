81659a1cb8e6e40ea2ca66fd8e3d824033965e6f
Dante Galindo Cruz <galindodantem@gmail.com>
2018-06-19 15:11:18 -0400

    adding content to the main areaalong with styling

--- js/script.js
+++ js/script.js
@@ -8,17 +8,15 @@ filter = document.querySelector('#filter');
 hamburgerMenu.addEventListener('click', function(e){
     leftMenu.style.transition = "transform 1s ease";
     leftMenu.style.transform= "translate(-150%)";
-    mainContentContainer.style.transition = "margin 1s ease";
-    mainContentContainer.style.margin = "0 15%";
-    mainContentContainer.style.transition = "width 1s ease";
+    mainContentContainer.style.margin = "5% 15% 0 15%";
+    mainContentContainer.style.transition = "width 0.8s ease";
     mainContentContainer.style.width= "70%";
 });
 
 mainContentMenu.addEventListener('click', function(e){
   leftMenu.style.transition = "transform 1s ease";
   leftMenu.style.transform= "translate(0)";
-  mainContentContainer.style.transition = "margin 1s ease";
-  mainContentContainer.style.margin = " 0 10%";
-  mainContentContainer.style.transition = "width 1s ease";
-  mainContentContainer.style.width= "60%";
+  mainContentContainer.style.margin = "5% 15% 0% 15%;";
+  mainContentContainer.style.transition = "width 0.8s ease";
+  mainContentContainer.style.width= "55%";
 });
