52c93a3f36972ff9fd6c39e4cdee94035483ea8e
Dante Galindo Cruz <galindodantem@gmail.com>
2018-06-19 09:37:59 -0400

    Making changes left menu functionality

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 hamburgerMenu = document.querySelector('.hamburger-menu');
-hamburgerLines = document.querySelectorAll('.hamburger');
+//hamburgerLines = document.querySelectorAll('.hamburger');
 leftMenu = document.querySelector('#left-menu');
 mainContent = document.querySelector('#main-content');
 filter = document.querySelector('#filter');
 
 hamburgerMenu.addEventListener('click', function(e){
-    if(leftMenu.style.width=="16.5%"){
-    leftMenu.style.transition = "width 1s";
-    leftMenu.style.width= 0;
+    if(leftMenu.style.transform == 'translate(0px)') {
+    leftMenu.style.transition = "transform 1s ease";
+    leftMenu.style.transform= "translate(-150%)";
     hamburgerMenu.style.position= 'fixed';
-    hamburgerMenu.style.width = '1.5%';
+    hamburgerMenu.style.width = '1.8%';
+    mainContent.appendChild(hamburgerMenu);
  } else{
-   leftMenu.style.width='16.5%';
+   leftMenu.style.transition = "transform 1s ease";
+   leftMenu.style.transform= "translate(0)";
  }
 })
