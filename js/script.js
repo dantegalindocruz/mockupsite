1c2a6608a3850e98ad13b5c3015f55482b65f5c2
Dante Galindo Cruz <galindodantem@gmail.com>
2018-06-19 10:42:31 -0400

    adding nav links and styling

--- js/script.js
+++ js/script.js
@@ -1,18 +1,15 @@
 hamburgerMenu = document.querySelector('.hamburger-menu');
 //hamburgerLines = document.querySelectorAll('.hamburger');
 leftMenu = document.querySelector('#left-menu');
-mainContent = document.querySelector('#main-content');
+mainContentMenu = document.querySelector('#main-content .hamburger-menu');
 filter = document.querySelector('#filter');
 
 hamburgerMenu.addEventListener('click', function(e){
-    if(leftMenu.style.transform == 'translate(0px)') {
     leftMenu.style.transition = "transform 1s ease";
     leftMenu.style.transform= "translate(-150%)";
-    hamburgerMenu.style.position= 'fixed';
-    hamburgerMenu.style.width = '1.8%';
-    mainContent.appendChild(hamburgerMenu);
- } else{
-   leftMenu.style.transition = "transform 1s ease";
-   leftMenu.style.transform= "translate(0)";
- }
-})
+});
+
+mainContentMenu.addEventListener('click', function(e){
+  leftMenu.style.transition = "transform 1s ease";
+  leftMenu.style.transform= "translate(0)";
+});
