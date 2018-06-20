31762c66b4bacc4a3aa57daec45897c33f9b0f43
Dante Galindo Cruz <galindodantem@gmail.com>
2018-06-20 15:45:18 -0400

    Adding mobile queries

--- js/script.js
+++ js/script.js
@@ -7,15 +7,26 @@ filter = document.querySelector('#filter');
 hamburgerMenu.addEventListener('click', function(e){
     leftMenu.style.transition = "transform 1s ease";
     leftMenu.style.transform= "translate(-150%)";
+    if(window.matchMedia("(min-width: 300px) and (max-width: 800px)").matches){
+      mainContentContainer.style.width= "80%";
+      mainContentContainer.style.margin="5% 10% 0% 10%";
+    } else{
     mainContentContainer.style.margin = "5% 15% 0 15%";
     mainContentContainer.style.transition = "width 0.8s ease";
     mainContentContainer.style.width= "70%";
+  }
 });
 
 mainContentMenu.addEventListener('click', function(e){
   leftMenu.style.transition = "transform 1s ease";
   leftMenu.style.transform= "translate(0)";
+
+  if(window.matchMedia("(min-width: 300px) and (max-width: 500px)").matches){
+    mainContentContainer.style.width= "60%";
+    mainContentContainer.style.margin="5% 10% 0% 10%";
+  } else{
   mainContentContainer.style.margin = "5% 15% 0% 15%;";
   mainContentContainer.style.transition = "width 0.8s ease";
   mainContentContainer.style.width= "55%";
+  }
 });
