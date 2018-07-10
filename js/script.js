321169cfdc3b80371468ea7dd9372ca9a809a646
Dante Galindo Cruz <galindodantem@gmail.com>
2018-07-10 13:46:57 -0400

    Adding missing div tags and customizing css and js for mobile

--- js/script.js
+++ js/script.js
@@ -10,7 +10,6 @@ leftHamburgerMenu.addEventListener('click', function(e){
     if(window.matchMedia("(min-width: 300px) and (max-width: 800px)").matches){
       mainContentContainer.style.width= "80%";
       mainContentContainer.style.margin="5% 10% 0% 10%";
-      leftMenu.style.overflow="hidden";
     } else{
     mainContentContainer.style.margin = "5% 15% 0 15%";
     mainContentContainer.style.transition = "width 0.8s ease";
@@ -22,7 +21,7 @@ mainContentMenu.addEventListener('click', function(e){
   leftMenu.style.transition = "transform 1s ease";
   leftMenu.style.transform= "translate(0)";
 
-  if(window.matchMedia("(min-width: 300px) and (max-width: 500px)").matches){
+  if(window.matchMedia("(min-width: 300px) and (max-width: 800px)").matches){
     mainContentContainer.style.width= "80%";
     mainContentContainer.style.margin="5% 10% 0% 10%";
   } else{
